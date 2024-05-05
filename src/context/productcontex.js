bb69a6ed5f414de25d56492bec8c6f13b333b8bd
Pranjal Jaiswal <amitm.webito@gmail.com>
2024-05-05 19:29:25 +0530

    first commit

---
+++ src/context/productcontex.js
@@ -0,0 +1,63 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import reducer from "../reducer/productReducer";
+
+const AppContext = createContext();
+
+const API = "https://6513aef88e505cebc2ea0df2.mockapi.io/AllItems ";
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+  isSingleLoading: false,
+  singleProduct: {},
+};
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
+  };
+
+  // my 2nd api call for single product
+
+  const getSingleProduct = async (id) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
+    try {
+      const res = await axios.get(API);
+      // const singleProduct = await res.data;
+      const singleProduct = res.data.find(product => product.id === id);
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
+    }
+
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state, getSingleProduct }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hooks
+const useProductContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, AppContext, useProductContext };
